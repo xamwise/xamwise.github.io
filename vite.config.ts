f6a99d435017962290e24f34a443a3893a66917f
xamwise <64870626+xamwise@users.noreply.github.com>
2024-02-06 12:41:29 +1100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: CONFIG.base || '/',
+  base: '/',
   plugins: [
     react(),
     createHtmlPlugin({
